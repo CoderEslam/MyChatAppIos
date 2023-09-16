551eff4fdfa43833d07f77ecc3034a2001ec24c1
CoderEslam <eslamghazy600@gmail.com>
2023-09-16 16:42:11 +0200

    Initial Commit

---
+++ ChatApp/ChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppApp.swift
+//  ChatApp
+//
+//  Created by Eslam Ghazy on 16/9/23.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
